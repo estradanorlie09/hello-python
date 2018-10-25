5a030a94caa0329034647df34a89e4c8ff47afb2
Norlie Estrada <estradanorlie@gmail.com>
2018-10-26 01:00:09 +0800

    ex3

---
+++ estrada_e3.py
@@ -0,0 +1,4 @@
+input = input("Enter a comma separated list of numbers:")
+x=1
+while x < 5:
+     if x & 1:
---
+++ try.py
@@ -0,0 +1,9 @@
+# for c in "hello!":
+#     print (c)
+numbers = []
+for x in range(10):
+    numbers.append(x)
+    print(numbers)
+
+    numbers2 =[x for x in range(10)]
+    print(numbers2)
