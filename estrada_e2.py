5ab92ac202fc6b61eddd374b316b0835b6e46860
Norlie Estrada <estradanorlie@gmail.com>
2018-10-26 21:11:19 +0800

    e2

--- estrada_e2.py
+++ estrada_e2.py
@@ -1,3 +1,16 @@
 import sys
 
-thunder = sys.argv[1]
+g = sys.argv[1]
+g = int(sys.argv[1])
+
+if g >= 200:
+    print("Super Typhoon")
+elif g <= 61:
+    print("Tropical Depression")
+elif g == 118 or g <=220:
+    print("typhoon")
+elif g == 89 or g <= 117:
+    print("severe tropical storm")
+
+elif g == 62 or g <= 88:
+    print("Tropical Storm")
